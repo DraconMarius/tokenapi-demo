67dfbe163c783e37e1955c8769b2f260b91fb26a
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2024-05-19 12:58:52 -0400

    init, base structure resembling nft api

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const routes = require('./controllers')
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes)
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`))
