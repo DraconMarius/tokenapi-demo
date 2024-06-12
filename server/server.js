c6d1adb8fb99cc2feb1f9553d473a354b914fa8a
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2024-06-12 16:04:14 -0400

    re-added catch all routes

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes)
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// })
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`))
